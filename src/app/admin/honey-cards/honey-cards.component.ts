a22567d05242fa23116b269131b064799f207c5e
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-10-03 09:17:44 +0200

    Improve honeysCards

--- src/app/admin/honey-cards/honey-cards.component.ts
+++ src/app/admin/honey-cards/honey-cards.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faWarehouse } from '@fortawesome/free-solid-svg-icons';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HoneyModalComponent } from '../honey-modal/honey-modal.component';
 import { Honey } from 'src/app/shared/honey';
+import { HoneysService } from 'src/app/services/honeys.service';
 
 
 @Component({
@@ -13,17 +14,22 @@ import { Honey } from 'src/app/shared/honey';
 export class HoneyCardsComponent implements OnInit {
 
   faAngleRight = faAngleRight;
+  faWarehouse = faWarehouse;
 
-  honeys: Honey[] = [
-    {id: 1, name: 'Acacia Honey', amount: 1000, price: 30},
-    {id: 2, name: 'Avocado Honey', amount: 500, price: 40},
-    {id: 3, name: 'Basswood Honey', amount: 300, price: 35},
-    {id: 4, name: 'Blueberry Honey', amount: 99, price: 33},
-  ];
+  honeys: Honey[];
+  //  = [
+  //   {id: 1, name: 'Acacia Honey', amount: 1000, price: 30},
+  //   {id: 2, name: 'Avocado Honey', amount: 500, price: 40},
+  //   {id: 3, name: 'Basswood Honey', amount: 300, price: 35},
+  //   {id: 4, name: 'Blueberry Honey', amount: 99, price: 33},
+  // ];
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private honeysService: HoneysService) { }
 
   ngOnInit(): void {
+    this.honeysService.getHoneysList().subscribe(x => {
+      this.honeys = x.body;
+    });
   }
 
   open(honey: Honey) {
@@ -31,6 +37,7 @@ export class HoneyCardsComponent implements OnInit {
     modalRef.componentInstance.honeyName = honey.name;
     modalRef.componentInstance.amount = honey.amount;
     modalRef.componentInstance.price = honey.price;
+   // modalRef.componentInstance.price = honey.image;
   }
 
 }
